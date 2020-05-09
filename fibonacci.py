343799cdca5d49119102c9e11c57addb60edd085
Balaraju583 <64912950+Balaraju583@users.noreply.github.com>
2020-05-09 20:09:21 +0530

    program by me

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 19:59:32 2020
+
+@author: ADMIN
+"""
+
+
+x = int(input("Enter the num: "))
+n1=0
+n2=1
+y=0
+for i in range(0,x+1):
+  print(n1)
+  y = n1+n2
+  n1=n2
+  n2=y
